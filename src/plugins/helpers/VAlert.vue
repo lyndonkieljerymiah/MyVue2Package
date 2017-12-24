1cb67a6c3cbfb42558a0ad0e67646dd5cac01aa5
= <nabcogroup@gmail.com>
2017-12-24 16:42:07 +0300

    update alert

--- src/plugins/helpers/VAlert.vue
+++ src/plugins/helpers/VAlert.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id="vAlertDialog" class="modal fade" role="dialog">
-      <div class="modal-dialog modal-md">
+    <div id="vAlertDialog" class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog modal-md" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" @click="close(false)" aria-label="Close"><span
